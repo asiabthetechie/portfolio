757c34746ed643a971146c703f7290e7c2938350
Asia Bragg <asiambragg987@gmail.com>
2021-02-14 22:37:01 -0800

    Added more star decor that needs to be properly organized.

--- src/components/SocialMediaContainer/SocialMediaContainer.js
+++ src/components/SocialMediaContainer/SocialMediaContainer.js
@@ -7,14 +7,22 @@ const SocialMediaContainer = () => {
 
     // Profile Pic
     const profilePicture = "/portfolio-app/src/assets/images/profile-pic256px.png";
+    const stars = "/portfolio-app/src/assets/misc/three-stars128px.png";
 
     return (
         <div className="socialMediaContainer">
+            {/* Header Section above Buttons*/}
+            <img classname="right-stars"
+                src={stars}/>
+            <img className="left-stars"
+                src={stars}/>
             <img className="profilePic" 
                 src={profilePicture}/>
             <div className="connectText">
-                <span>Connect with me on any of the socials below!</span>
+                <span>Connect with me on any of my socials below!</span>
             </div>
+
+            {/*Social Media Button Area*/}
             <SocialMediaButtons />
         </div>
     )

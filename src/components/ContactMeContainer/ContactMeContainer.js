d855d36c88df50b068c96ef9d95a55b70a397f33
Asia Bragg <asiambragg987@gmail.com>
2021-02-23 20:23:40 -0800

    Updated the contact form, organized HTML and added more CSS

--- src/components/ContactMeContainer/ContactMeContainer.js
+++ src/components/ContactMeContainer/ContactMeContainer.js
@@ -5,6 +5,8 @@ import ContactMeForm from "../ContactMeForm/ContactMeForm";
 const ContactMeContainer = () => {
     return (
         <div className="contactMeContainer">
+            <h1 className="contactMeHeader">Contact Me</h1>
+            <span>If you want to contact me directly for assistance with your next project please fill out the contact form below.</span>
             <ContactMeForm />
         </div>
     )
--- src/components/ContactMeForm/ContactMeForm.js
+++ src/components/ContactMeForm/ContactMeForm.js
@@ -3,63 +3,122 @@ import "./ContactMeForm.css";
 
 const ContactMeForm = () => {
     return (
-        <div>
-            <h1>Contact Me</h1>
-            <span>Fill out the form below to contact me directly for your next project.</span>
-
-            {/*Start of the Form*/}
-            <form action="" className="contactForm">
-                {/*Name*/}
-                <fieldset>
-                <legend>Name</legend>
-                <label for="fname">First Name:</label>
-                <input type="text" 
-                        id="fname" 
-                        name="firstname" 
-                        placeholder="First Name..."/>
-                <label for="lname">Last Name:</label>
-                <input type="text" 
-                        id="lname" 
-                        name="lastname" 
-                        placeholder="Last Name..."/>
-                </fieldset>
-
+        <div className="contactFormContainer">
+            {/*Contact Me Form*/}
+            <form>
                 {/*Contact Info*/}
                 <fieldset>
-                <legend>Contact Info</legend>
-                <label for="email">Email:</label>
-                <input type="text" 
-                        id="email" 
-                        placeholder="Email..."/>
-                <label for="cname">Company</label>
-                <input type="text" 
-                        id="cname" 
-                        name="companyname" 
-                        placeholder="Please include your company name..."/>
+                    <legend>Contact Info</legend>
+                    <div className="contactInfo">
+                        {/*First Name*/}
+                        <div className="firstName">
+                            <label htmlFor="fname">First Name <br/>
+                                <input type="text" 
+                                        id="fname" 
+                                        name="firstname"/>
+                            </label>
+                        </div>
+                        
+                        {/*Last Name*/}
+                        <div className="lastName">
+                            <label htmlFor="lname">Last Name <br/>
+                                <input type="text" 
+                                        id="lname" 
+                                        name="lastname"/>
+                            </label>
+                        </div>    
+
+                        {/*Email*/}
+                        <div className="emailContainer">
+                            <label htmlFor="email">Email <br/>
+                                <i>Enter a email for response.</i><br/>
+                                <input type="text" 
+                                        id="email" />
+                            </label>
+                        </div>
+
+                        {/*Company Name*/}
+                        <div className="companyName">
+                            <label htmlFor="cname">Company <br/>
+                                <i>Enter your company name here.</i><br/>
+                                <input type="text" 
+                                    id="cname" 
+                                    name="companyname" />
+                            </label>
+                        </div>
+                    </div>
                 </fieldset>
 
-                {/*Project Info*/}
+                {/*Job Info*/}
                 <fieldset>
-                <legend>Project Info</legend>
-                <label for="ptitle">Project Title:</label>
-                <input type="text"
-                        id="ptitle"
-                        placeholder="Enter your working project title..."/>
-                <label for="pjobrole">Desired Job Role:</label>
-                <select id="pjobrole" name="pjobrole">
-                    <option value="website-dev">Website Devleoper</option>
-                    <option value="web-app-dev">Web App Developer</option>
-                    <option value="mobile-app-dev">Mobile App Developer</option>
-                    <option value="video-game-dev">Video Game Developer</option>
-                    <option value="video-editor">Video Editor</option>
-                </select>
-                <textarea name="pdesc">
-                    Please tell me about your project and the development services the project requires. 
-                </textarea>
+                    <legend>Job Info</legend>
+                    <div className="jobInfo">
+                        {/*Primary Job Task*/}
+                        <div className="job-task job-info">
+                            <label htmlFor="jtask">Primary Job Task <br/>
+                                <i>Enter the primary job task..</i> <br/>
+                                <input type="text"
+                                        id="jtask" />
+                            </label>
+                        </div>
+
+                        {/*Service*/}
+                        <div className="service job-info">
+                            <label htmlFor="service">Specific Service <br/>
+                                <i>Select the services you require. One required, three maximum.</i><br/>
+                                <input type="checkbox" id="webdesign" name="service" value="web-design"/>
+                                    <label htmlFor="webdesign">Web Design</label>
+
+                                <input type="checkbox" id="webdevelopment" name="service" value="web-development"/>   
+                                    <label htmlFor="webdevelopment">Web Development</label>
+
+                                <input type="checkbox" id="maintenance" name="service" value="web-maintenance"/>
+                                    <label htmlFor="maintenance">Web Maintenance</label>
+                                
+                                <br/>
+
+                                <input type="checkbox" id="consulting" name="service" value="consultation"/>
+                                    <label htmlFor="consultation">Consultation</label>
+
+                                <input type="checkbox" id="gamedevelopment" name="service" value="game-development"/>   
+                                    <label htmlFor="gamedevelopment">Game Development</label>
+
+                                <input type="checkbox" id="videoediting" name="service" value="video-editing"/>   
+                                    <label htmlFor="videoediting">Video Editing</label>
+                            </label>
+                        </div>
+
+                        {/*Job Role*/}
+                        <div className="job-role job-info">
+                            <label htmlFor="jobrole">Job Roles <br/>
+                                <i>Select the job role you believe best fits the primary task.</i><br/>
+                                <input type="radio" id="jobrole1" name="jobrole" value="consultant"/>
+                                    <label htmlFor="jobrole1">Consultant</label>
+                                <input type="radio" id="jobrole2" name="jobrole" value="designer"/>
+                                    <label htmlFor="jobrole2">Designer</label> 
+                                <input type="radio" id="jobrole3" name="jobrole" value="developer"/>
+                                    <label htmlFor="jobrole3">Developer</label>
+                                <input type="radio" id="jobrole4" name="jobrole" value="editor"/>
+                                    <label htmlFor="jobrole4">Editor</label>
+                            </label>
+                        </div>
+                        
+                        
+                        {/*Project Description*/}
+                        <div className="job-info">
+                            <label htmlFor="pdesc">Project Description</label><br/>
+                                <i>Give a brief description of the job/project. No more than 500 characters please.</i><br/>
+                                <textarea id="pdesc" name="pdesc">
+                                    No need to get detailed, one sentence is fine.
+                                </textarea>
+                        </div>
+                    </div>        
                 </fieldset>
 
                 {/*Submit Button*/}
-                <input type="submit" value="Submit"/>
+                <div className="submitButton">
+                    <input type="submit" value="Submit"/>
+                </div>
             </form>
         </div>
     )

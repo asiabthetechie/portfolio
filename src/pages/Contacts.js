766fb4696e638673711f1bca781aec19f4aed0eb
Asia Bragg <asiambragg987@gmail.com>
2021-03-16 20:26:41 -0700

    Making some final changes before putting my portfolio live

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import SocialMediaContainer from '../components/SocialMediaContainer/SocialMediaContainer';
-import shootingStarsPic from "../../src/assets/misc/shooting-stars-svg.svg";
 import './Contacts.css';
 
 const Contacts = () => {
